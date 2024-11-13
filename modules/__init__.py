b6e3ead2a327ed9ea386e46dfaf9ad203a4ce9ce
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-11-12 19:16:57 -0500

    Add Init

---
+++ modules/__init__.py
@@ -0,0 +1,10 @@
+from .analyzer_rbac import Analyze_RBAC
+from .analyzer_rugar import Analyze_RUGAR
+from .compare_data import Compare_Data
+
+
+__all__ = [
+    Analyze_RBAC, 
+    Analyze_RUGAR, 
+    Compare_Data
+]
