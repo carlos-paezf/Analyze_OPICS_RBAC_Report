1b86725c0e25d0971c84acac69539c43d1ee732f
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-11-12 19:17:25 -0500

    Add Main Script

---
+++ main.py
@@ -0,0 +1,18 @@
+import warnings
+
+from modules import Analyze_RUGAR, Analyze_RBAC, Compare_Data
+
+
+warnings.filterwarnings(
+    action="ignore",
+    category=UserWarning, 
+    module="openpyxl"
+)
+
+
+if __name__ == '__main__':
+    opics_data = Analyze_RUGAR().users_info
+        
+    rbac_data = Analyze_RBAC().rbac_data
+
+    Compare_Data(opics_data, rbac_data).compare_users()
