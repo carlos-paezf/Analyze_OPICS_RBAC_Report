68fd0112054e333abdaef59a84b3c9aff8a4f02f
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-11-13 13:26:52 -0500

    "Added new method `compare_users_in_reports` to `Compare_Data` class, removed empty `compare_users` method, and imported `array_diff` function from `utils` module."

--- modules/compare_data.py
+++ modules/compare_data.py
@@ -1,4 +1,4 @@
-from utils import measure_run_time
+from utils import measure_run_time, array_diff
 
 
 class Compare_Data():
@@ -8,5 +8,18 @@ class Compare_Data():
 
 
     @measure_run_time
-    def compare_users(self):
-        pass
+    def compare_users_in_reports(self) -> tuple[list, list]:
+        """
+        This function compares users in two reports and returns lists of users that are present in one
+        report but not in the other.
+        :return: The function `compare_users_in_reports` returns a tuple containing two lists. The first
+        list contains users from the OPICS data that are not present in the RBAC data, and the second
+        list contains users from the RBAC data that are not present in the OPICS data.
+        """
+        opics_users = [i["usuario_opics"] for i in self.opics_data]
+        rbac_users = [i["usuario_opics"] for i in self.rbac_data["perfil-usuario"]]
+
+        opics_not_in_rbac = array_diff(sorted(opics_users), sorted(rbac_users))
+        rbac_not_in_opics = array_diff(sorted(rbac_users), sorted(opics_users))
+
+        return opics_not_in_rbac, rbac_not_in_opics
