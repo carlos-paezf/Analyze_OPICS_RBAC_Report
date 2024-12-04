1a358529b021860d1b5072178f4a2549ccc1bd94
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-12-04 14:55:46 -0500

    "Removed calls to compare_data methods and added import of App_GUI module, instantiated App_GUI and called its mainloop method."

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import warnings
 
-from modules import Analyze_RUGAR, Analyze_RBAC, Compare_Data
-
+from modules import Analyze_RBAC, Analyze_RUGAR, App_GUI, Compare_Data
 
 warnings.filterwarnings(
     action="ignore",
@@ -15,10 +14,4 @@ if __name__ == '__main__':
     rbac = Analyze_RBAC()
     compare_data = Compare_Data(rugar, rbac)
 
-    [opics_not_in_rbac, rbac_not_in_opics] = compare_data.compare_users_in_reports()
-    reports_diff = compare_data.compare_users_groups()
-    compare_data.compare_profiles_groups()
-
-    # print("opics_not_in_rbac: ", opics_not_in_rbac)
-    # print("rbac_not_in_opics: ", rbac_not_in_opics)
-    # print("reports_diff: ", reports_diff)
+    App_GUI(compare_data).mainloop()
