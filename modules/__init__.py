b22a1780a3b3de5f55a27d7f5146ed2979f588c3
Carlos David Paez Ferreira <cpaezferreira@gmail.com>
2024-12-04 14:54:21 -0500

    "Added App_GUI to __init__.py imports and __all__ list"

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,10 +1,12 @@
 from .analyzer_rbac import Analyze_RBAC
 from .analyzer_rugar import Analyze_RUGAR
+from .app_gui import App_GUI
 from .compare_data import Compare_Data
 
 
 __all__ = [
     Analyze_RBAC, 
     Analyze_RUGAR, 
+    App_GUI,
     Compare_Data
 ]
